3988f72c2bbb3fff0111e815ba7cb9c439b0357d
Taurus0205 <dolchin1990@mail.ru>
2021-08-29 09:46:34 +0500

    Add modal project

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useRef } from "react";
+
+import "./App.scss";
+
+// images
+import MrBean from "./Assests/Images/english.jpg";
+import ashamed from "./Assests/Images/ashamed.png";
+
+function App() {
+  const elModal = useRef(null);
+  return (
+    <>
+      <div className="bored">
+        <p className="bored__paragraph">
+          Bored? Nothing to do? Wanna play something interesting? Try this
+          button...
+        </p>
+        <button
+          className="bored__btn"
+          type="button"
+          onClick={() => {
+            elModal.current.classList.add("modal--active");
+          }}
+        >
+          Start PUBG
+        </button>
+      </div>
+
+      <div className="modal" ref={elModal}>
+        <div className="modal-inner">
+          <img
+            className="modal__img"
+            src={MrBean}
+            alt="Mr Bean"
+            width={200}
+            height={100}
+          />
+          <button
+            className="modal__btn"
+            onClick={() => {
+              elModal.current.classList.remove("modal--active");
+            }}
+          >
+            Oops. You got me
+            <img
+              className="modal__ashamed"
+              src={ashamed}
+              alt="ashamed"
+              width={35}
+              height={35}
+            />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
